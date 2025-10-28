47bfd573f61692b16b02e0f07431508fe6c10859
diamite01-webr <whappermatthew@gmail.com>
2025-10-28 15:04:50 +0100

    Initialize example/orm-prisma

---
+++ src/bot/context.ts
@@ -0,0 +1,28 @@
+import type { Config } from '#root/config.js'
+import type { Logger } from '#root/logger.js'
+import type { PrismaClientX } from '#root/prisma/index.js'
+import type { AutoChatActionFlavor } from '@grammyjs/auto-chat-action'
+import type { HydrateFlavor } from '@grammyjs/hydrate'
+import type { I18nFlavor } from '@grammyjs/i18n'
+import type { ParseModeFlavor } from '@grammyjs/parse-mode'
+import type { Context as DefaultContext, SessionFlavor } from 'grammy'
+
+export interface SessionData {
+  // field?: string;
+}
+
+interface ExtendedContextFlavor {
+  logger: Logger
+  config: Config
+  prisma: PrismaClientX
+}
+
+export type Context = ParseModeFlavor<
+  HydrateFlavor<
+    DefaultContext &
+    ExtendedContextFlavor &
+    SessionFlavor<SessionData> &
+    I18nFlavor &
+    AutoChatActionFlavor
+  >
+>
